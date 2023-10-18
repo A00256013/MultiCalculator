6b418e89094993280564e57b03d8964a87183274
vishwatrivedi <a00256013@mycambrian.ca>
2023-10-18 15:57:58 -0400

    Enhanced code pushed

--- androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.multicalculator.android
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -12,9 +15,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.multicalculator.Greeting
 
 
 class MainActivity : ComponentActivity() {
@@ -26,102 +33,94 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView(Greeting().greet())
-
-                    // Call the CalcView function and display its result
-                    CalcDisplay(CalcView())
-
-                    // Call the CalcNumericButton function to create numeric buttons
-                    Row {
-                        CalcNumericButton(number = 1) {
-                            // Handle button click for 1
-                        }
-                        CalcNumericButton(number = 2) {
-                            // Handle button click for 2
-                        }
-                        // Add more numeric buttons as needed
-                    }
-
-                    // Call the CalcOperationButton function to create operation buttons
-                    Row {
-                        CalcOperationButton(operation = "+") {
-                            // Handle button click for addition
-                        }
-                        CalcOperationButton(operation = "-") {
-                            // Handle button click for subtraction
-                        }
-                        // Add more operation buttons as needed
-                    }
-
-                    // Call the CalcEqualsButton function to create the "=" button
-                    CalcEqualsButton {
-                        // Handle button click for calculating the result
-                    }
+                    DefaultPreview()
                 }
             }
         }
     }
+}
 
-    private fun CalcNumericButton(number: Int, onClick: () -> Unit) {
-        TODO("Not yet implemented")
-    }
-
-    private fun CalcOperationButton(operation: String, onClick: () -> Unit) {
-        TODO("Not yet implemented")
-    }
-
-    private fun CalcEqualsButton(onClick: () -> Unit) {
-
+@Preview
+@Composable
+fun DefaultPreview() {
+    MyApplicationTheme {
+        CalcView()
     }
 }
+
 @Composable
 fun CalcView() {
+    val displayText = remember { mutableStateOf("0") }
 
-}
-
-@Composable
-fun CalcDisplay(calcView: Any) {
-
-}
-@Composable
-fun CalcOperationButton(operation: String, display: MutableState<String>, onClick: () -> Unit) {
-    Button(
-        onClick = {
-            display.value += operation // Update the display with the selected operation
-            onClick()
-        },
-        modifier = Modifier.padding(4.dp) // Set padding to 4 dp
+    Column(
+        modifier = Modifier.background(Color.LightGray)
     ) {
-        Text(text = operation) // Use the operation parameter value
-    }
-}
-@Composable
-fun CalcNumericButton(number: Int, display: MutableState<String>, onClick: () -> Unit) {
-    Button(
-        onClick = {
-            display.value += number.toString() // Append the selected number to the display
-            onClick()
-        },
-        modifier = Modifier.padding(4.dp) // Set padding to 4 dp
-    ) {
-        Text(text = number.toString()) // Use the number parameter value
+        CalcDisplay(displayText)
+        Row {
+            Column {
+                for (i in 7 downTo 1 step 3) {
+                    CalcRow(displayText, i, 3)
+                }
+                Row {
+                    CalcNumericButton(0, displayText)
+                    CalcEqualsButton(displayText)
+                }
+            }
+            Column {
+                CalcOperationButton("+", displayText)
+                CalcOperationButton("-", displayText)
+                CalcOperationButton("*", displayText)
+                CalcOperationButton("/", displayText)
+            }
+        }
     }
 }
 
 @Composable
-fun CalcEqualsButton(display: MutableState<String>, onClick: () -> Unit) {
+fun CalcRow(display: MutableState<String>, startNum: Int, numButtons: Int) {
+    val endNum = startNum + numButtons
+    Row(modifier = Modifier.padding(0.dp)) {
+        for (i in startNum until endNum) {
+            CalcNumericButton(i, display)
+        }
+    }
+}
+
+@Composable
+fun CalcDisplay(display: MutableState<String>) {
+    Text(
+        text = display.value,
+        modifier = Modifier
+            .height(50.dp)
+            .fillMaxWidth()
+            .padding(5.dp),
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+fun CalcNumericButton(number: Int, display: MutableState<String>) {
     Button(
-        onClick = {
-            display.value = "0" // Set the display value to "0"
-            onClick()
-        },
-        modifier = Modifier.padding(4.dp) // Add padding of 4 dp
+        onClick = { display.value += number.toString() },
+        modifier = Modifier.padding(4.dp)
+    ) {
+        Text(text = number.toString())
+    }
+}
+
+@Composable
+fun CalcOperationButton(operation: String, display: MutableState<String>) {
+    Button(onClick = {}, modifier = Modifier.padding(4.dp)) {
+        Text(text = operation)
+    }
+}
+
+@Composable
+fun CalcEqualsButton(display: MutableState<String>) {
+    Button(
+        onClick = { display.value = "0" },
+        modifier = Modifier.padding(4.dp)
     ) {
         Text(text = "=")
     }
-}
-
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
 }
--- androidApp/src/main/java/com/example/multicalculator/android/MyApplicationTheme.kt
+++ androidApp/src/main/java/com/example/multicalculator/android/MyApplicationTheme.kt
@@ -1,12 +1,14 @@
 package com.example.multicalculator.android
 
+//import androidx.compose.material.MaterialTheme
+//import androidx.compose.material.Shapes
+//import androidx.compose.material.Typography
+//import androidx.compose.material.darkColors
+//import androidx.compose.material.lightColors
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Shapes
 import androidx.compose.material3.Typography
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -21,20 +23,20 @@ fun MyApplicationTheme(
     content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) {
-        darkColorScheme(
+        darkColors(
             primary = Color(0xFFBB86FC),
-            secondary = Color(0xFF03DAC5),
-            tertiary = Color(0xFF3700B3)
+            primaryVariant = Color(0xFF3700B3),
+            secondary = Color(0xFF03DAC5)
         )
     } else {
-        lightColorScheme(
+        lightColors(
             primary = Color(0xFF6200EE),
-            secondary = Color(0xFF03DAC5),
-            tertiary = Color(0xFF3700B3)
+            primaryVariant = Color(0xFF3700B3),
+            secondary = Color(0xFF03DAC5)
         )
     }
     val typography = Typography(
-        bodyMedium = TextStyle(
+        TextStyle(
             fontFamily = FontFamily.Default,
             fontWeight = FontWeight.Normal,
             fontSize = 16.sp
@@ -52,4 +54,16 @@ fun MyApplicationTheme(
         shapes = shapes,
         content = content
     )
+}
+
+fun MaterialTheme(colorScheme: Any, typography: Typography, shapes: Shapes, content: @Composable () -> Unit) {
+    TODO("Not yet implemented")
+}
+
+fun darkColors(primary: Color, primaryVariant: Color, secondary: Color): Any {
+    TODO("Not yet implemented")
+}
+
+fun lightColors(primary: Color, primaryVariant: Color, secondary: Color): Any {
+    TODO("Not yet implemented")
 }
