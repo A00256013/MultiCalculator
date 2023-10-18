c936b8fd8a9839b44e06cc33996605d0bcb4015d
vishwatrivedi <a00256013@mycambrian.ca>
2023-10-17 22:22:41 -0400

    CalcNumericButton function created

--- androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.multicalculator.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.multicalculator.Greeting
 
 class MainActivity : ComponentActivity() {
@@ -25,6 +29,17 @@ class MainActivity : ComponentActivity() {
 
                     // Call the CalcView function and display its result
                     CalcDisplay(CalcView())
+
+                    // Call the CalcNumericButton function to create numeric buttons
+                    Row {
+                        CalcNumericButton(number = 1) {
+                            // Handle button click for 1
+                        }
+                        CalcNumericButton(number = 2) {
+                            // Handle button click for 2
+                        }
+                        // Add more buttons as needed
+                    }
                 }
             }
         }
@@ -50,6 +65,16 @@ fun CalcRow(value: String) {
 @Composable
 fun CalcDisplay(calculation: String) {
     Text(text = calculation)
+}
+
+@Composable
+fun CalcNumericButton(number: Int, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Text(text = number.toString())
+    }
 }
 
 @Preview
