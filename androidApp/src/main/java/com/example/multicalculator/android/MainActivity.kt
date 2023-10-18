00402aed9108a05d7f51cb111de261c78dd128e4
vishwatrivedi <a00256013@mycambrian.ca>
2023-10-17 22:29:53 -0400

    CalcOperationButton function created

--- androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
@@ -5,16 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.multicalculator.Greeting
+
+class CalcNumericButton(number: Int, function: () -> Unit) {
+
+}
+
+class CalcView {
+
+}
+
+class GreetingView(greet: String) {
+
+}
+
+class CalcDisplay(calcView: CalcView) {
+
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,51 +46,10 @@ class MainActivity : ComponentActivity() {
                             // Handle button click for 1
                         }
                         CalcNumericButton(number = 2) {
-                            // Handle button click for 2
                         }
-                        // Add more buttons as needed
                     }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
-@Composable
-fun CalcView(): String {
-    // Perform your calculations or operations here
-    return "Calculations result"
-}
-
-@Composable
-fun CalcRow(value: String) {
-    Text(text = value)
-}
-
-@Composable
-fun CalcDisplay(calculation: String) {
-    Text(text = calculation)
-}
-
-@Composable
-fun CalcNumericButton(number: Int, onClick: () -> Unit) {
-    Button(
-        onClick = onClick,
-        modifier = Modifier.padding(8.dp)
-    ) {
-        Text(text = number.toString())
-    }
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
     }
 }
